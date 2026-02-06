3e9d086c6fbefacf8608e198324688610984562a
leojay <leojay.cause.of.500@gmail.com>
2026-02-06 09:40:44 +0800

    init

---
+++ src/main/kotlin/com/leojay/simplewgad/SimpleWgAdApplication.kt
@@ -0,0 +1,11 @@
+package com.leojay.simplewgad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleWgAdApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleWgAdApplication>(*args)
+}
---
+++ src/test/kotlin/com/leojay/simplewgad/SimpleWgAdApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leojay.simplewgad
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleWgAdApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
