8d47735f3e539cfe45a9c1d262912cf20a8f03bd
leojay <leojay.cause.of.500@gmail.com>
2026-02-07 15:22:31 +0800

    fix: 切换可见性按钮不生效

--- src/main/kotlin/com/leojay/simplewgad/controller/LoginController.kt
+++ src/main/kotlin/com/leojay/simplewgad/controller/LoginController.kt
@@ -1,12 +1,27 @@
 package com.leojay.simplewgad.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class LoginController {
     @GetMapping("/login")
     fun loginPage(): String {
         return "login"
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    fun login(@RequestParam password : String): String {
+        if (password == "12345678asd") {
+            return "success"
+        }else {
+            return "fail"
+        }
+
     }
 }
